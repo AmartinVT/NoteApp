0c7ca1ca0002d3e05ac73f935e639812cdf74f19
VT_AOE_91 <amartin80108@gmail.com>
2022-07-22 11:03:36 -0400

    Added port, fixed GET

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const express = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 const notes = require('Develop/public/assets/js/index.js')
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +13,10 @@ app.use(express.json());
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/db/db.json'));
-});
-
-app.get('/api/notes', (req, res) => res.json(notes));
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
