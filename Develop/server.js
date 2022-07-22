c5672ea2e36ce1280285621bbd799d81e9f73de8
VT_AOE_91 <amartin80108@gmail.com>
2022-07-22 10:55:17 -0400

    Added express statements to GET and listen >>> NOT working

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,20 @@ const fs = require('fs');
 const express = require('fs');
 const path = require('path');
 const uuid = require('uuid');
+const notes = require('Develop/public/assets/js/index.js')
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Develop/db/db.json'));
+});
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
