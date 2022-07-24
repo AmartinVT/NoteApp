65563cd66bfdb01dada0595bd126ce82a6d369be
VT_AOE_91 <amartin80108@gmail.com>
2022-07-24 15:59:55 -0400

    Fixed static url >>> note page displaying correctly

--- Develop/server.js
+++ Develop/server.js
@@ -2,27 +2,26 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-//const uuid = require('uuid');
-//const notes = require('Develop/public/assets/js/index.js');
 const util = require('util');
-const PORT = process.env.PORT || 3001;
+
 
 // Creates app to utilize express for local server management
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Add a static middleware for serving assets in the public folder
-app.use(express.static('/public'));
+app.use(express.static("./public"));
 
 // Reading and writing
 const read = util.promisify(fs.readFile);
 const write = util.promisify(fs.writeFile);
 
 // GET Route for database
-app.get("/api/notes", (req, res) => {
+app.get("api/notes", (req, res) => {
   read("/db/db.json","UTF-8").then(function (data){
     notes = [].concat(JSON.parse(data))
     res.json(notes);
@@ -45,15 +44,15 @@ app.post("/api/notes", (req, res) => {
 
 // HTML Route for homepage
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname,"public/notes.html"));
+  res.sendFile(path.join(__dirname,"/public/notes.html"));
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,"public/index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname,"/public/index.html"));
 });
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname,"public/index.html"));
+  res.sendFile(path.join(__dirname,"/public/index.html"));
 });
 
 // App listening to the port
