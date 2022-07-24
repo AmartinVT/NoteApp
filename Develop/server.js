4b67d23855feec6fedb68da63c1fbb7c51adda53
VT_AOE_91 <amartin80108@gmail.com>
2022-07-24 14:46:12 -0400

    Fixed CSS error via Chrome setting

--- Develop/server.js
+++ Develop/server.js
@@ -34,7 +34,11 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname,"public/notes.html"));
 });
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,"public/index.html"));
+});
+
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname,"public/index.html"));
 });
 
