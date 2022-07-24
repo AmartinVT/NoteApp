daa237ccf3d804fcbce576e9fb22891f54b47da5
VT_AOE_91 <amartin80108@gmail.com>
2022-07-24 12:12:39 -0400

    Fixed get statement >>> still unable to GET

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const express = require('fs');
 const path = require('path');
 const uuid = require('uuid');
-const notes = require('Develop/public/assets/js/index.js');
+//const notes = require('Develop/public/assets/js/index.js');
 const util = require('util');
 const PORT = process.env.PORT || 3001;
 
@@ -19,7 +19,7 @@ const read = util.promisify(fs.readFile);
 const write = util.promisify(fs.writeFile);
 
 // GET Route for homepage
-app.get('Develop/public/notes.html', (req, res) => {
+app.get("/api/notes", (req, res) => {
   read("Develop/db/db.json","utf-8")
 }
   
