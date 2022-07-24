9e1d999ad660afbccdccd47b106aca2cf84f1886
VT_AOE_91 <amartin80108@gmail.com>
2022-07-24 13:22:58 -0400

    Put index.html back to initial state. Began adding HTML route.

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 //const uuid = require('uuid');
 //const notes = require('Develop/public/assets/js/index.js');
 const util = require('util');
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5501;
 
 // Creates app to utilize express for local server management
 const app = express();
@@ -15,20 +15,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Add a static middleware for serving assets in the public folder
-app.use(express.static("Develop/public"));
+app.use(express.static("/public"));
 
 // Reading and writing
-const getNotes = util.promisify(fs.readFile);
+const read = util.promisify(fs.readFile);
 const write = util.promisify(fs.writeFile);
 
-// GET Route for homepage
+// GET Route for database
 app.get("/api/notes", (req, res) => {
-  getNotes("./Develop/db/db.json","utf-8").then(function (data){
+  read("./Develop/db/db.json","utf-8").then(function (data){
     notes = [].concat(JSON.parse(data))
     res.json(notes);
   })
 }
-  
+ 
+// HTML Route for homepage
+
 );
 
 // App listening to the port
